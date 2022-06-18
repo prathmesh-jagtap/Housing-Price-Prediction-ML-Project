bfe109765f1f0c96b62477cee5dc6fc5daf8dc33
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2022-06-18 15:08:00 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, request, redirect
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
-    return "<h1>Starting Machine Learning Project</h1>"
+    try:
+        raise Exception("Testing")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message)
+        logging.info("Testing 2")
+    return "<h1>CI/CD pipeline has established</h1>"
 
 if __name__=="__main__":
     app.run(debug=True)

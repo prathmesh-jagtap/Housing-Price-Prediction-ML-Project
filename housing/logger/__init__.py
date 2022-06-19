a72e35e64a3cf50e7b9fb914ff4b2f14b3d261a6
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2022-06-19 18:58:32 +0530

    updated

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -11,7 +11,7 @@ LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
 
 os.makedirs(LOG_DIR,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
 
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode="w",

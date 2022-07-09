46f117f0af190376f0be294bae85228a0950c83b
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2022-07-09 20:44:07 +0530

    updated code

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -1,5 +1,11 @@
 import os
 from datetime import datetime
+
+
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+
 ROOT_DIR = os.getcwd()  # to get current working directory
 CONFIG_DIR = "config"
 CONFIG_FILE_NAME = "config.yaml"
@@ -16,7 +22,6 @@ TRAINING_PIPELINE_NAME_KEY = "pipeline_name"
 
 
 # Data Ingestion related variable
-
 DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
 DATA_INGESTION_ARTIFACT_DIR = "data_ingestion"
 DATA_INGESTION_DOWNLOAD_URL_KEY = "dataset_download_url"
@@ -25,6 +30,7 @@ DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
 DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+
 
 # Data Validation related variables
 DATA_VALIDATION_CONFIG_KEY = "data_validation_config"

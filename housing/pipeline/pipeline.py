94030e279bcc120d4be092022a20daccb9c58904
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2022-07-09 21:12:44 +0530

    update

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -32,7 +32,7 @@ config = Configuartion()
 os.makedirs(config.training_pipeline_config.artifact_dir, exist_ok=True)
 
 
-class Pipeline:
+class Pipeline(Thread):
     experiment: Experiment = Experiment(*([None] * 11))
 
     experiment_file_path = os.path.join(config.training_pipeline_config.artifact_dir,
@@ -40,9 +40,8 @@ class Pipeline:
 
     def __init__(self, config: Configuartion = config) -> None:
         try:
-            super().__init__(name="pipeline")
+            super().__init__(daemon=False, name="pipeline")
             self.config = config
-
         except Exception as e:
             raise HousingException(e, sys) from e
 
@@ -51,18 +50,16 @@ class Pipeline:
             data_ingestion = DataIngestion(
                 data_ingestion_config=self.config.get_data_ingestion_config())
             return data_ingestion.initiate_data_ingestion()
-
         except Exception as e:
             raise HousingException(e, sys) from e
 
-    def start_data_validation(self,
-                              data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) \
+            -> DataValidationArtifact:
         try:
             data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
                                              data_ingestion_artifact=data_ingestion_artifact
                                              )
             return data_validation.initiate_data_validation()
-
         except Exception as e:
             raise HousingException(e, sys) from e
 
@@ -77,23 +74,19 @@ class Pipeline:
                 data_validation_artifact=data_validation_artifact
             )
             return data_transformation.initiate_data_transformation()
-
         except Exception as e:
             raise HousingException(e, sys)
 
-    def start_model_trainer(self,
-                            data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
         try:
             model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
                                          data_transformation_artifact=data_transformation_artifact
                                          )
             return model_trainer.initiate_model_trainer()
-
         except Exception as e:
             raise HousingException(e, sys) from e
 
-    def start_model_evaluation(self,
-                               data_ingestion_artifact: DataIngestionArtifact,
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
                                data_validation_artifact: DataValidationArtifact,
                                model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
         try:
@@ -103,18 +96,16 @@ class Pipeline:
                 data_validation_artifact=data_validation_artifact,
                 model_trainer_artifact=model_trainer_artifact)
             return model_eval.initiate_model_evaluation()
-
         except Exception as e:
             raise HousingException(e, sys) from e
 
-    def start_model_pusher(self,  model_eval_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+    def start_model_pusher(self, model_eval_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
         try:
             model_pusher = ModelPusher(
                 model_pusher_config=self.config.get_model_pusher_config(),
                 model_evaluation_artifact=model_eval_artifact
             )
             return model_pusher.initiate_model_pusher()
-
         except Exception as e:
             raise HousingException(e, sys) from e
 
@@ -181,7 +172,6 @@ class Pipeline:
                                              )
             logging.info(f"Pipeline experiment: {Pipeline.experiment}")
             self.save_experiment()
-
         except Exception as e:
             raise HousingException(e, sys) from e
 
